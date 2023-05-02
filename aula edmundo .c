7991695455c39842e66f62322e5399d9af8180a6
Yasmin Moura <131416864+mourayasmin@users.noreply.github.com>
2023-05-02 20:46:16 -0300

    Update aula edmundo .c

--- aula edmundo .c
+++ aula edmundo .c
@@ -14,6 +14,7 @@ int main() {
     
 return 0;
 }
+----------------------------------------------------------------------------------------------
 #include <stdio.h>
 
 int main() {
@@ -27,5 +28,23 @@ int main() {
   valornovo = valor*10 + a4;
   printf("O NOVO NUMERO E = %d\n",valornovo);
 
+  return 0;
+}
+
+-----------------------------------------------------------------------------------------------
+#include <stdio.h>
+#include <math.h>
+
+int main () {
+  int x1, y1, x2, y2, calc;
+  float distab;
+  scanf ("%d", &x1);
+  scanf ("%d", &y1);
+  scanf ("%d", &x2);
+  scanf ("%d", &y2);
+  calc = pow((x2-x1),2) + pow((y2-y1),2);
+  distab = sqrt(calc);
+  printf("A DISTANCIA ENTRE A E B = %.2f\n", distab); \\ .2f para valor decimal com 2 casas
+  
   return 0;
 }
