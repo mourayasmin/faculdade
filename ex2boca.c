21f1e1cea98a87d5c96d398a051ad8c7867c5dd7
Yasmin Moura <131416864+mourayasmin@users.noreply.github.com>
2023-05-03 13:52:01 -0300

    Create ex2boca.c

---
+++ ex2boca.c
@@ -0,0 +1,13 @@
+#include <stdio.h>  // ler o valor do salário mínimo e a qtd de kw gasta 
+int main () {
+  float kw, consumo, desconto;
+  scanf("%.2f",&kw)("%.2f",&consumo)("%.2f", &desconto);
+  res = (0.57, 
+  
+    
+  
+  
+  
+  
+  return0;
+}
