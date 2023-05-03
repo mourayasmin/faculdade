21ffda48dfc7294257a19ddac119efc4c5a10bfe
Yasmin Moura <131416864+mourayasmin@users.noreply.github.com>
2023-05-03 20:39:41 -0300

    Update ex2boca.c

--- ex2boca.c
+++ ex2boca.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 int main () {
   int salario;
-  float kw, r, rr, rrr;
-  scanf("%d %f", &salario, &kw); //entrada: f; saída: .2f
+  float kW, r, rr, rrr;
+  scanf("%d %f", &salario, &kW); //entrada: f; saída: .2f
    r = (salario*0.007);
-   rr = (salario*0.007*kw);
-   rrr = (salario*0.007*kw*0.9);    
-   printf("Custo por kw: R$%.2f\n", r);
+   rr = (salario*0.007*kW);
+   rrr = (salario*0.007*kW*0.9);    
+   printf("Custo por kW: R$%.2f\n", r);
    printf("Custo do consumo: R$%.2f\n", rr);
    printf("Custo com desconto: R$%.2f\n", rrr); 
    
   return 0;
-
+}
