c23317f26c5a328ed9b44a3963dbf36889be7bf6
Yasmin Moura <131416864+mourayasmin@users.noreply.github.com>
2023-05-10 11:32:21 -0300

    ex9.c

---
+++ ex9.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main (){
+  int horas, minutos, segundos, tempo;
+  scanf("%d", &horas);
+  scanf("%d", &minutos);
+  scanf("%d", &segundos);
+  tempo = (horas*60*60) + (minutos*60) + (segundos);
+  printf("O TEMPO EM SEGUNDOS E = %d", tempo);
+  
+  return 0;
+}
