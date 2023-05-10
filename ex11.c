61f38cd8ed28a53875743445258f90ad234608d9
Yasmin Moura <131416864+mourayasmin@users.noreply.github.com>
2023-05-10 19:30:07 -0300

    ex11.c

---
+++ ex11.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+int main () {
+  float l1, l2, l3, area, t;
+  scanf("%f\n", &l1);
+  scanf("%f\n", &l2);
+  scanf("%f", &l3);
+  area = sqrt(t(t-l1)(t-l2)(t-l3));
+  t = (l1+l2+l3)/2;
+  printf("A AREA DO TRIANGULO E = %.2f\n", area);
+  
+return 0;
+}
+
