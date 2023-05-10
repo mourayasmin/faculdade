3bcb50b732837e001a78da99ab9238bd8c26d0a8
Yasmin Moura <131416864+mourayasmin@users.noreply.github.com>
2023-05-10 17:48:09 -0300

    ex10meu.c

--- exercicio10.c
+++ exercicio10.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 int main (){
-   float valor;
-   scanf("%.6f", &valor);
+   float valor, resposta;
+   int inteiro;
    
-  
-  
-  
-  
+   scanf("%f", &valor);
+   
+   inteiro = (valor*10.0 + 0.5); // 10.0 para que a divisão seja fracionária
+   resposta = (inteiro/10.0);
+   printf("%.6f\n", resposta);
+   
+   inteiro = (valor*100.0 + 0.5);
+   resposta = (inteiro/100.0);
+   printf("%.6f\n", resposta);
+   
+   inteiro = (valor*1000.0 + 0.5);
+   resposta = (inteiro/1000.0);
+   printf("%.6f\n", resposta);
   
   return 0;
 }
+
