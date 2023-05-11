7d4d53b54d770dc34b9a4f71e8518df215e2da29
Yasmin Moura <131416864+mourayasmin@users.noreply.github.com>
2023-05-11 00:16:29 -0300

    ex15.c

---
+++ ex15.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main (){
+  int n0(123), n1(1), n2(2), n3(3), n4(calculo), novo;
+  scanf("%d", &n0);
+  n1 = (n0/100); // = 1
+  n2 = (n0)/(n0/2); // = 2
+  n3 = (n0 - n1*100 - n2*10); // = 3
+  novo = (n1*1000 + n2*100 + n3*10) +  
+  
+  
+  
+  
+return 0;
+}
