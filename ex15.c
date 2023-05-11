ad6ce4d14c7dd663727c61520027a80dbf326fb2
Yasmin Moura <131416864+mourayasmin@users.noreply.github.com>
2023-05-11 00:28:59 -0300

    ex15.c

--- ex15.c
+++ ex15.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 int main (){
-  int n0(123), n1(1), n2(2), n3(3), n4(calculo), novo;
+  int n0, n1, n2, n3, novo;
   scanf("%d", &n0);
-  n1 = (n0/100); // = 1
-  n2 = (n0)/(n0/2); // = 2
-  n3 = (n0 - n1*100 - n2*10); // = 3
-  novo = (n1*1000 + n2*100 + n3*10) +  
-  
-  
-  
+  n1 = (n0/100);
+  n2 = (n0)/(n0/2); 
+  n3 = (n0 - n1*100 - n2*10); 
+  novo = (n1*1000 + n2*100 + n3*10) + (n1 + n2*3 + n3*5)%7; 
+  printf("O NOVO NUMERO E = %d\n", novo);
   
 return 0;
 }
