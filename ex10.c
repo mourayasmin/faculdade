c67605250b0a48a1912811ee609e126836df9465
Yasmin Moura <131416864+mourayasmin@users.noreply.github.com>
2023-05-10 23:58:00 -0300

    ex10.c

--- ex10.c
+++ ex10.c
@@ -17,3 +17,28 @@ int main() {
 
     return 0;
 }
+
+
+-------------------------------------------------------------------------------
+    
+#include <stdio.h>
+int main (){
+   float valor, resposta;
+   int inteiro;
+   
+   scanf("%f", &valor);
+   
+   inteiro = (valor*10.0 + 0.5); // 10.0 para que a divisão seja fracionária
+   resposta = (inteiro/10.0);
+   printf("%.6f\n", resposta);
+   
+   inteiro = (valor*100.0 + 0.5);
+   resposta = (inteiro/100.0);
+   printf("%.6f\n", resposta);
+   
+   inteiro = (valor*1000.0 + 0.5);
+   resposta = (inteiro/1000.0);
+   printf("%.6f\n", resposta);
+  
+  return 0;
+}
