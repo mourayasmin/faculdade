b177a2b544a7a2171f18793ea1e25cb15465d707
Yasmin Moura <131416864+mourayasmin@users.noreply.github.com>
2023-05-11 10:55:53 -0300

    ex16.c

--- ex16.c
+++ ex16.c
@@ -11,9 +11,12 @@ int main (){
   n7 = (n6)%2;
   n8 = (n7)%2;
   binario = (n1n2n3n4n5n6n7n8);
-   if 0 <= n <= 255  
-  
-  
+   if (0 <= n <= 255) {
+     printf("%d", binario);
+   }    
+  else {
+     printf("Numero invalido!");
+   }
   
  return 0; 
 }
