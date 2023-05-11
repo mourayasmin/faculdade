52ec8f44febd99c39ce50877122656eb8d304ddc
Yasmin Moura <131416864+mourayasmin@users.noreply.github.com>
2023-05-11 07:52:35 -0300

    ex16.c

---
+++ ex16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main (){
+  int n, binario;
+  scanf("%d", &n);
+  n1 = (n)%2;
+  n2 = (n1)%2;
+  n3 = (n2)%2;
+  n4 = (n3)%2;
+  n5 = (n4)%2;
+  n6 = (n5)%2;
+  n7 = (n6)%2;
+  n8 = (n7)%2;
+  binario = (n1n2n3n4n5n6n7n8);
+   if 0 <= n <= 255  
+  
+  
+  
+ return 0; 
+}
