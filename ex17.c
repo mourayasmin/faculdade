1a82c11a791403228addfa148e25074709b7439e
Yasmin Moura <131416864+mourayasmin@users.noreply.github.com>
2023-05-11 19:09:35 -0300

    ex17.c

--- ex17.c
+++ ex17.c
@@ -1,1 +1,11 @@
 #include <stdio.h>
+int main () {
+  int n, n1, n2, n3;
+  scanf("%d", &n);
+  n1 = (n/100); //1
+  n2 = (n/10 - n1*10);
+  n3 = (n - n1*100 - n2*10);
+  printf("%d%d%d\n", n3, n2, n1);
+  
+ return 0; 
+}
