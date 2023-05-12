37457af43c5faeed6a11c5aa6859647cd8b81830
Yasmin Moura <131416864+mourayasmin@users.noreply.github.com>
2023-05-12 10:37:37 -0300

    ex19.c

--- ex19.c
+++ ex19.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 int main () {
  float a, b, c, d, e, f, x, y;
-  scanf("%f", a);
-  scanf("%f", b);
-  scanf("%f", c);
-  scanf("%f", d);
-  scanf("%f", e);
-  scanf("%f", f);
-  c = a*x + b*y; // x = a, b, c, d, e, f ; y = a, b, c, d, e, f 
-  f = d*x + e*y;
+  scanf("%f", &a);
+  scanf("%f", &b);
+  scanf("%f", &c);
+  scanf("%f", &d);
+  scanf("%f", &e);
+  scanf("%f", &f);
+  y = (a*f - d*c)/(-d*b + a*e);
+  x = (e*c - b*f)/(-b*d + e*a);
   printf("O VALOR DE X E = %.2f\n", x);
   printf("O VALOR DE Y E = %.2f\n", y);
  
