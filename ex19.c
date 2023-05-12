9c1c41f9ff228fe001187dcfab865fe795602e41
Yasmin Moura <131416864+mourayasmin@users.noreply.github.com>
2023-05-11 21:17:27 -0300

    ex19.c

---
+++ ex19.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main () {
+ float a, b, c, d, e, f, x, y;
+  scanf("%f", a);
+  scanf("%f", b);
+  scanf("%f", c);
+  scanf("%f", d);
+  scanf("%f", e);
+  scanf("%f", f);
+  c = a*x + b*y;
+  f = d*x + e*y;
+  printf("O VALOR DE X E = %.2f\n", x);
+  printf("O VALOR DE Y E = %.2f\n", y);
+ 
+  return 0;
+}
