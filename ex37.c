91f2f9111570e5d8015d09357e6e19e2cef13f11
Yasmin Moura <131416864+mourayasmin@users.noreply.github.com>
2023-06-11 18:57:39 -0300

    ex37.c

---
+++ ex37.c
@@ -0,0 +1,27 @@
+#include <stdio.h> 
+int main () {
+int x, y, z;
+char A, B, C, nx, ny, nz;
+scanf("%d %d %d\n", &x, &y, &z);
+scanf("%c%c%c", &nx, &ny, &nz); 
+  // x - y - z ; nx == c , ny == b , nz == a
+  
+  
+  // x - z - y
+  
+  // y - x - z 
+  
+  // y - z - x 
+  
+  // z - y - x 
+  
+  // z - x - y 
+
+
+
+
+
+
+
+return 0; 
+}
