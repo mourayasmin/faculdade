7e3c20106a7204498637dd617c9fa32052c8d589
Yasmin Moura <131416864+mourayasmin@users.noreply.github.com>
2023-06-11 19:37:02 -0300

    Update ex37.c

--- ex37.c
+++ ex37.c
@@ -4,6 +4,23 @@ int x, y, z;
 char A, B, C, nx, ny, nz;
 scanf("%d %d %d\n", &x, &y, &z);
 scanf("%c%c%c", &nx, &ny, &nz); 
+  if (x > y && y > z) {
+  if (nx == 'C' && ny == 'B' && nz == 'C'){
+    printf("%d %d %d\n", x, y, z);
+  }
+  else if (nx == 'C' && ny == 'A' && nz == 'B') {
+printf("%d %d %d\n", x, z, y);
+  }
+  else if (nx == 'A' && ny == 'B' && nx == 'C') {
+printf("%d %d %d\n", z, y, x);
+  }
+else if 
+
+    
+
+
+    
+  
   // x - y - z ; nx == c , ny == b , nz == a
   
   
